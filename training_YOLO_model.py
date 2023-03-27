13279f2cb8d8823693eee7e38953f3e45b8a96e5
DeepakKabdwal <90624878+DeepakKabdwal@users.noreply.github.com>
2023-03-27 23:49:49 +0530

    trained the model
    the trained model is included

--- training_YOLO_model.py
+++ training_YOLO_model.py
@@ -3,4 +3,13 @@
 # repo_url = 'https://github.com/ultralytics/ultralytics.git'
 # clone_dir = './YOLOv8'
 # subprocess.run(["git", "clone", repo_url, clone_dir])
+import os
+from ultralytics import YOLO
+import multiprocessing as mp
+if __name__ == '__main__':
+    mp.set_start_method('spawn')
+
+
+model = YOLO('yolov8n.yaml')
+model.train(data='data.yaml', epochs=3)
 
