c326bd80edf39d1a0ca98e24c0a9fec81f8a1701
DeepakKabdwal <90624878+DeepakKabdwal@users.noreply.github.com>
2023-03-27 22:36:15 +0530

    done with data preparation part

--- extract_object_as_text.py
+++ extract_object_as_text.py
@@ -4,6 +4,7 @@ from functools import reduce
 import pandas as pd
 from xml.etree import ElementTree as et
 from concurrent.futures import ThreadPoolExecutor
+from shutil import move
 
 
 def extract_object_info(xml_file):
@@ -44,6 +45,12 @@ def convert_xml_to_yolo(xml_dir):
     df[cols_to_convert] = df[cols_to_convert].astype(float)
     return df
 
+def label_encoding(df):
+              'bottle':9, 'sheep':10, 'diningtable':11, 'horse':12, 'motorbike':13, 'sofa':14, 'cow':15, 'car':16, 'cat':17,
+              'bus':18, 'pottedplant':19}
+    df.loc[:, 'id'] = df.loc[:, 'name'].apply(lambda x: labels[x])
+    return df
+
 
 if __name__ == '__main__':
     xml_dir = './dataset/'
@@ -80,16 +87,35 @@ test_df = df.query(f'filename in {images_test}')
 #print(train_df.head())
 
 #encode the label names as we can't train on text values
-def label_encoding(df):
-    labels = {'person':0, 'aeroplane':1, 'tvmonitor':2, 'train':3, 'boat':4, 'dog':5, 'chair':6, 'bird':7, 'bicycle':8,
-              'bottle':9, 'sheep':10, 'diningtable':11, 'horse':12, 'motorbike':13, 'sofa':14, 'cow':15, 'car':16, 'cat':17,
-              'bus':18, 'pottedplant':19}
-    df.loc[:, 'label'] = df.loc[:, 'name'].apply(lambda x: labels[x])
-    return df
-
-
 train_df = label_encoding(train_df)
 test_df = label_encoding(test_df)
 
-print(test_df.head())
+train_folder = 'dataset/train'
+test_folder = 'dataset/test'
+
+os.mkdir(train_folder)
+os.mkdir(test_folder)
+cols = ['filename', 'id', 'center_x', 'center_y', 'w', 'h']
+training_group_by_obj = train_df[cols].groupby('filename')
+testing_group_by_obj = test_df[cols].groupby('filename')
+
+def save_data(filename, folder_path, group_obj):
+    src = os.path.join('dataset', filename)
+    dst = os.path.join(folder_path, filename)
+    move(src, dst)
+
+    text_filename = os.path.join(folder_path,
+                                 os.path.splitext(filename)[0] + '.txt')
+    group_obj.get_group(filename).set_index('filename').to_csv(text_filename, sep=' ',
+                                                               index=False, header = False)
+#moving training and testing files to respective directories
+filename_series = pd.Series(list(training_group_by_obj.groups.keys()))
+filename_series_test = pd.Series(list(testing_group_by_obj.groups.keys()))
+filename_series.apply(save_data, args=(train_folder, training_group_by_obj))
+filename_series_test.apply(save_data, args=(test_folder, testing_group_by_obj))
+
+
+
+
+
 
