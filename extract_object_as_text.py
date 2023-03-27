e55899b8cc2c4b7b308d49843206fe2dc96070e4
DeepakKabdwal <90624878+DeepakKabdwal@users.noreply.github.com>
2023-03-27 20:28:02 +0530

    started encoding data

--- extract_object_as_text.py
+++ extract_object_as_text.py
@@ -47,10 +47,39 @@ df = pd.DataFrame(data, columns=['filename', 'width', 'height', 'name', 'xmin', 
 cols = ['width', 'height', 'xmin', 'xmax', 'ymin', 'ymax']
 df[cols] = df[cols].astype(float)
 
+<<<<<<< Updated upstream
 # get yolo labels from this data
 df['center_x'] = ((df['xmax'] + df['xmin']) / 2) / df['width']
 df['center_y'] = ((df['ymax'] + df['ymin']) / 2) / df['height']
 df['w'] = (df['xmax'] - df['xmin']) / df['width']
 df['h'] = (df['ymax'] - df['ymin']) / df['height']
+=======
+if __name__ == '__main__':
+    xml_dir = './dataset/'
+    df = convert_xml_to_yolo(xml_dir)
+
+images = df['filename'].unique()
+#print(len(images))
+images_df = pd.DataFrame(images, columns=['FileNames'])
+# we need test and train split of 80% to 20%
+#print(images_df.head())
+images_train = tuple(images_df.sample(frac=0.8)['FileNames'])    #randomly take 80% of the dataframe
+#rest 20% as test images
+
+# NEVER train on test images
+images_test = tuple(images_df.query(f'FileNames not in {images_train}')['FileNames'])
+# print(len(images_df))
+# print(len(images_train))
+# print(len(images_test))
+#dataframes made for training and testing
+train_df = df.query(f'filename in {images_train}')
+test_df = df.query(f'filename in {images_test}')
+
+#print(train_df.head())
+
+#encode the label names as we can't train on text values
+
+
+>>>>>>> Stashed changes
 
 # print(df.info())
