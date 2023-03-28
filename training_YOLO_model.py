bfdc51ca9cf46bcd1a4f4e232b2cfcff8604738a
DeepakKabdwal <90624878+DeepakKabdwal@users.noreply.github.com>
2023-03-28 16:20:43 +0530

    what is this i don't even know it just said ther eare hanging changes
    Signed-off-by: DeepakKabdwal <90624878+DeepakKabdwal@users.noreply.github.com>

--- training_YOLO_model.py
+++ training_YOLO_model.py
@@ -4,12 +4,27 @@
 # clone_dir = './YOLOv8'
 # subprocess.run(["git", "clone", repo_url, clone_dir])
 import os
+import random
 from ultralytics import YOLO
 import multiprocessing as mp
 if __name__ == '__main__':
     mp.set_start_method('spawn')
 
 
-model = YOLO('yolov8n.yaml')
-model.train(data='data.yaml', epochs=1)
+model = YOLO('yolov8n.pt')
+test_folder = "C:/Users/ACER/PycharmProjects/Yolo-object-detection/dataset\\test\\"
+
+
+
+img_list = os.listdir(test_folder)
+img_file = random.choice(img_list)
+img_path = test_folder + img_file
+
+
+# Run detection on selected image
+results = model(img_path)
+
+# Print detection results
+results.show()
+
 
