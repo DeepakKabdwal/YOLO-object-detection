554b7a41ca87bb187c9ef4a23525d8e369e358ee
DeepakKabdwal <90624878+DeepakKabdwal@users.noreply.github.com>
2023-04-22 23:35:43 +0530

    what

--- training_YOLO_model.py
+++ training_YOLO_model.py
@@ -1,30 +1,7 @@
-#run this only once this is needed to get YOLOv8 from their repository
-# import subprocess
-# repo_url = 'https://github.com/ultralytics/ultralytics.git'
-# clone_dir = './YOLOv8'
-# subprocess.run(["git", "clone", repo_url, clone_dir])
-import os
-import random
 from ultralytics import YOLO
-import multiprocessing as mp
+from multiprocessing import freeze_support
+
 if __name__ == '__main__':
-    mp.set_start_method('spawn')
-
-
-model = YOLO('yolov8n.pt')
-test_folder = "C:/Users/ACER/PycharmProjects/Yolo-object-detection/dataset\\test\\"
-
-
-
-img_list = os.listdir(test_folder)
-img_file = random.choice(img_list)
-img_path = test_folder + img_file
-
-
-# Run detection on selected image
-results = model(img_path)
-
-# Print detection results
-results.show()
-
-
+    freeze_support()
+    model = YOLO('yolov8s.yaml')
+    model.train(data="C:\\Users\\ACER\\PycharmProjects\\Yolo-object-detection\\data.yaml", epochs=50)
